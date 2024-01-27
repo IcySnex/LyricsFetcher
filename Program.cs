1133e098d805e566b463e016f68118a354a13c88
IcySnex <81762750+IcySnex@users.noreply.github.com>
2024-01-27 18:54:07 +0100

    Updated to newest version of genius & fixed all bugs

--- Program.cs
+++ Program.cs
@@ -2,42 +2,66 @@
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net;
-
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 HttpClient Client = new();
 string Lyrics = "";
 
-Console.Write("Enter genius API key (get free one at 'https://docs.genius.com/'): ");
-string ApiKey = Console.ReadLine();
-if (string.IsNullOrEmpty(ApiKey)) Cancel("ApiKey is empty");
+//Console.Write("Enter genius API key (get free one at 'https://docs.genius.com/'): ");
+string? ApiKey = "ucRn7Z0zUGzwSe-qh3ulzELWMFhKEfiy61xpTJN81MbBiI75K4dB3eqTrsgON0yK";//Console.ReadLine();
+if (string.IsNullOrEmpty(ApiKey))
+    Cancel("ApiKey is empty");
 
 Console.Write("Enter Song Title: ");
-string SongTitle = Console.ReadLine();
-if (string.IsNullOrEmpty(SongTitle)) Cancel("SongTitle is empty");;
+string? SongTitle = Console.ReadLine();
+if (string.IsNullOrEmpty(SongTitle))
+    Cancel("SongTitle is empty"); ;
 Console.Write("Enter Song Artist: ");
-string SongArtist = Console.ReadLine();
-if (string.IsNullOrEmpty(SongArtist)) Cancel("SongArtist is empty");
+
+string? SongArtist = Console.ReadLine();
+if (string.IsNullOrEmpty(SongArtist))
+    Cancel("SongArtist is empty");
+
+
 Console.WriteLine("Searchig for Lyrics...");
 
-lyrics_search SearchResult = JsonConvert.DeserializeObject<lyrics_search>(await Client.GetStringAsync($"https://api.genius.com/search?q={SongTitle} {SongArtist}&access_token={ApiKey}"));
-if (SearchResult is null) Cancel("SearchResult is null");
-if (SearchResult.response.hits.Count == 0) Cancel("SearchResult has no hits");
+lyrics_search? SearchResult = JsonConvert.DeserializeObject<lyrics_search>(await Client.GetStringAsync($"https://api.genius.com/search?q={SongTitle} {SongArtist}&access_token={ApiKey}"));
+if (SearchResult is null)
+    Cancel("SearchResult is null");
+
+if (SearchResult!.response.hits.Count == 0)
+    Cancel("SearchResult has no hits");
+
+
 Console.WriteLine("Found Hit.");
 
 var Html = new HtmlDocument();
 Html.LoadHtml((await Client.GetStringAsync(SearchResult.response.hits[0].result.url)).Replace("https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js", ""));
 
-var Clickables = Html.DocumentNode.SelectNodes("//a[contains(@class, 'ReferentFragmentVariantdesktop')]");
-if (Clickables != null)
-    foreach (HtmlNode Node in Clickables)
-        Node.ParentNode.ReplaceChild(Html.CreateTextNode(Node.ChildNodes[0].InnerHtml), Node);
-var Spans = Html.DocumentNode.SelectNodes("//span");
-if (Spans != null)
-    foreach (HtmlNode Node in Spans)
-        Node.Remove();
-
 var Nodes = Html.DocumentNode.SelectNodes("//div[@data-lyrics-container]");
-Lyrics = WebUtility.HtmlDecode(string.Join("", Nodes.Select(Node => Node.InnerHtml)).Replace("<br>", "\n"));
+
+string GetChildrenDirectText(HtmlNodeCollection nodes)
+{
+    StringBuilder builder = new();
+    foreach (HtmlNode child in nodes)
+    {
+        if (child.HasChildNodes)
+        {
+            builder.Append(GetChildrenDirectText(child.ChildNodes));
+            continue;
+        }
+
+        string innerText = child.GetDirectInnerText();
+        if (!string.IsNullOrWhiteSpace(innerText))
+            builder.AppendLine(innerText);
+    }
+
+    return builder.ToString();
+}
+
+Lyrics = WebUtility.HtmlDecode(Regex.Replace(GetChildrenDirectText(Nodes), @"\[(.*?)\]", "\n$&").Trim());
 Console.WriteLine("Lyrics fetched!\n");
 
 Console.ForegroundColor = ConsoleColor.Green;
@@ -62,11 +86,11 @@ void Cancel(string Reason)
 
 
 public partial class lyrics_search
-{ 
+{
     public lyrics_search_response response { get; set; }
 }
 public partial class lyrics_search_response
-{ 
+{
     public List<lyrics_search_response_hits> hits { get; set; }
 }
 public partial class lyrics_search_response_hits
